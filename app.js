a2d02bf52e880f15c8bd9d33cd078d72894ca27f
matthova <matthova87@gmail.com>
2015-06-21 02:39:38 +0000

    autoconnect port is stable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var monitor = require('node-usb-detection');
 var serialport = require('serialport');
+var child_process = require('child_process');
 
 var vid = 0x16c0;
 var pid = 0x0483;
 var baud = 230400;
-var OPEN_TIMEOUT = 100; // milliseconds
+var portName = '/dev/ttyACM';
+var OPEN_TIMEOUT = 1000; // milliseconds
 var ourPort = undefined;
 
 devices = monitor.list();
@@ -22,63 +24,69 @@ monitor.remove(function(device) {
     findOurPort(device);
 });
 
-function ourDevice(device) {
+function findOurPort(device) {
     if (
 	Number(device.deviceDescriptor.idVendor) === vid
 	&& 
 	Number(device.deviceDescriptor.idProduct) === pid
     ) {
-	return true;
+	setTimeout(function() {
+	    open(device);
+	}, OPEN_TIMEOUT);
     }
-    return false;
 }
 
-function findOurPort(device) {
-    if(ourDevice(device)){
-	serialport.list(function (err, ports) {
-	    console.log("ports!", ports);
-	    /*
-	    ports.forEach(function(port) {
-		if (
-		    Number(device.deviceDescriptor.idVendor) === Number(port.vendorId)
-		    &&
-		    Number(device.deviceDescriptor.idProduct) === Number(port.productId)
-		) {
-		    if (ourPort === undefined) {
-			console.log("opening port");
-			open(port);
-		    } else {
-			console.log("port removed");
-			ourPort.close();
-			ourPort = undefined;
-		    }
-		}
-	    });
-	    */
+function getPort(portNumber) {
+    code = child_process.execSync('ls ' + portName + portNumber).toString().split("\n")[0];
+    console.log("this is the port from command line", code);
+    console.log(code === portName + portNumber);
+    return code;
+}
+
+function open() {
+    var portNumber = 0;
+    if(ourPort === undefined) {
+	tryPort(portNumber);
+    } else {
+	ourPort.close(function() {
+	    console.log("closed port!");
+	    ourPort = undefined;
 	});
     }
 }
 
-function open(port) {
-    ourPort = new serialport.SerialPort(port.comName, {
-	baudrate: baud
-    }, false); // this is the openImmediately flag [default is true]
-
-    ourPort.open(function (error) {
-	if ( error ) {
-	    console.log('failed to open: ' + error);
-	} else {
-	    ourPort.on('data', function(data) {
-		console.log('data received: ' + data);
+function tryPort(portNumber) {
+    try {
+	if(getPort(portNumber) === portName + portNumber) {
+	    ourPort = new serialport.SerialPort(
+		portName + portNumber,
+		{ baudrate: baud },
+		false
+	    ); // this is the openImmediately flag [default is true]
+	    console.log("here 1");
+	    ourPort.open(function (error) {
+		if ( error ) {
+		    console.log('failed to open: ' + error);
+		} else {
+		    ourPort.on('data', function(data) {
+			console.log('data received: ' + data);
+		    });
+		    console.log("here 2");
+		    setTimeout(function() {
+			ourPort.write("M501\n", function(err, results) {
+			    console.log('err ' + err);
+			    console.log('results ' + results);
+			});
+		    }, OPEN_TIMEOUT);
+		}
 	    });
-	    setTimeout(function() {
-		ourPort.write("M501\n", function(err, results) {
-		    console.log('err ' + err);
-		    console.log('results ' + results);
-		});
-	    }, OPEN_TIMEOUT);
 	}
-    });
+    } catch(ex) {
+	if(portNumber < 255) {
+	    tryPort(portNumber + 1);
+	}
+	console.log("No port available at", portName + portNumber);
+    }
 }
 
 process.on('exit', function() {
