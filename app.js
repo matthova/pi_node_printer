b5ec79d7a525fd610cd7cee8811d082b529d641c
matthova <matthova87@gmail.com>
2015-06-21 06:03:36 +0000

    stable class structure

--- app.js
+++ app.js
@@ -3,93 +3,99 @@ var serialport = require('serialport');
 var child_process = require('child_process');
 
 var Hydraprint = function (deviceData) {
-    var VID = 0x16c0;
-    var PID = 0x0483;
-    var BAUDRATE = 230400;
-    var PORT_NAME = '/dev/ttyACM';
-    var OPEN_TIMEOUT = 1000; // milliseconds
+    var that = this;
+    this.VID = 0x16c0;
+    this.PID = 0x0483;
+    this.BAUDRATE = 230400;
+    this.PORT_NAME = '/dev/ttyACM';
+    this.OPEN_TIMEOUT = 1000; // milliseconds
+    this.ourPort = undefined;
+
+    devices = monitor.list();
+    devices.forEach(function(device) {
+	that.findOurPort(device);
+    });
+
+    monitor.add(function(device) {
+	that.findOurPort(device);
+    });
+
+    monitor.remove(function(device) {
+	that.findOurPort(device);
+    });
 }
 
-var ourPort = undefined;
-
-devices = monitor.list();
-devices.forEach(function(device) {
-    findOurPort(device);
-});
-
-monitor.add(function(device) {
-    findOurPort(device);
-});
-
-monitor.remove(function(device) {
-    findOurPort(device);
-});
-
-function findOurPort(device) {
+Hydraprint.prototype.findOurPort = function(device) {
+    var that = this;
     if (
-	Number(device.deviceDescriptor.idVendor) === VID
+	Number(device.deviceDescriptor.idVendor) === that.VID
 	&& 
-	Number(device.deviceDescriptor.idProduct) === PID
+	Number(device.deviceDescriptor.idProduct) === that.PID
     ) {
 	setTimeout(function() {
-	    open();
-	}, OPEN_TIMEOUT);
+	    that.open();
+	}, that.OPEN_TIMEOUT);
     }
-}
+};
 
-function getPort(portNumber) {
-    code = child_process.execSync('ls ' + PORT_NAME + portNumber).toString().split("\n")[0];
-    console.log("code", code);
+Hydraprint.prototype.getPort = function (portNumber) {
+    code = child_process.execSync('ls ' + this.PORT_NAME + portNumber).toString().split("\n")[0];
     return code;
-}
+};
 
-function open() {
+Hydraprint.prototype.open = function() {
     var portNumber = 0;
-    if(ourPort === undefined) {
-	tryPort(portNumber);
+    var that = this;
+    if(that.ourPort === undefined) {
+	that.tryPort(portNumber);
     } else {
-	ourPort.close(function() {
+	that.ourPort.close(function() {
 	    console.log("port closed");
-	    ourPort = undefined;
+	    that.ourPort = undefined;
 	});
     }
-}
+};
 
-function tryPort(portNumber) {
+Hydraprint.prototype.tryPort = function(portNumber) {
+    var that = this;
     try {
-	if(getPort(portNumber) === PORT_NAME + portNumber) {
-	    ourPort = new serialport.SerialPort(
-		PORT_NAME + portNumber,
-		{ baudrate: BAUDRATE },
+	if(that.getPort(portNumber) === that.PORT_NAME + portNumber) {
+	    that.ourPort = new serialport.SerialPort(
+		that.PORT_NAME + portNumber,
+		{ baudrate: that.BAUDRATE },
 		false
 	    );
-	    ourPort.open(function (error) {
+	    that.ourPort.open(function (error) {
 		if ( error ) {
 		    console.log('failed to open: ' + error);
 		} else {
-		    ourPort.on('data', function(data) {
+		    that.ourPort.on('data', function(data) {
 			console.log('data received: ' + data);
 		    });
 		    setTimeout(function() {
-			ourPort.write("M501\n", function(err, results) {
-			    console.log('err ' + err);
-			    console.log('results ' + results);
+			that.ourPort.write("M501\n", function(err, results) {
+			    
 			});
-		    }, OPEN_TIMEOUT);
+		    }, that.OPEN_TIMEOUT);
 		}
 	    });
 	}
     } catch(ex) {
-	ourPort = undefined;
+	that.ourPort = undefined;
 	if(portNumber < 255) {
-	    tryPort(portNumber + 1);
+	    that.tryPort(portNumber + 1);
 	}
-	console.log("No port available at", PORT_NAME + portNumber);
+	console.log("No port available at", that.PORT_NAME + portNumber);
     }
 }
 
-process.on('exit', function() {
-    if(ourPort) {
-	ourPort.close();
-    }
-});
+Hydraprint.prototype.cleanup = function() {
+    var that = this;
+    process.on('exit', function() {
+	if(that.ourPort) {
+	    that.ourPort.close();
+	}
+    });
+};
+
+var printer = new Hydraprint();
